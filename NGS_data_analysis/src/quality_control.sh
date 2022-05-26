173eb15e9237be071a8398b28f761bdd896707ae
Anita Brzoza <anita_brzoza@wp.pl>
2022-05-26 09:49:46 +0200

    add quality control sript

---
+++ NGS_data_analysis/src/quality_control.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#================================================================================
+#
+# The script contains commands to generate:
+# 1) separate quality control reports on sample data using FastQC
+# 2) a quality control summary report using MultiQC
+#
+#================================================================================
+
+echo -e "\nGenerating separate quality control reports on samples ==========================\n"
+fastqc ../raw_data/*.fastq -o .
+
+echo -e "\nGenerating a quality control summary report =====================================\n"
+multiqc . -o .
