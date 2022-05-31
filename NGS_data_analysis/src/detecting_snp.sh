9993fb68a4ac9a7811a459f75df3d7fc60f72614
Anita Brzoza <anita_brzoza@wp.pl>
2022-05-31 13:46:28 +0200

    update detecting.sh

--- NGS_data_analysis/src/detecting_snp.sh
+++ NGS_data_analysis/src/detecting_snp.sh
@@ -13,15 +13,15 @@ for file in 5 6 7
     do
         echo -e "\nSearching polymorphisms for SRR06454${file}..."
         gatk HaplotypeCaller \
-            -R /home/abrzoza/Pulpit/reference_genome/reference_genome.fna \
-            -I /home/abrzoza/Pulpit/reference_genome/SRR06454$file.sorted.rg.md.bam \
+            -R ../reference_genome/reference_genome.fna \
+            -I ../reference_genome/SRR06454$file.sorted.rg.md.bam \
             -O SRR06454$file.vcf.gz \
             -ERC GVCF
     done
 
 echo -e "\nMerging files..."
 gatk CombineGVCFs \
-    -R /home/abrzoza/Pulpit/reference_genome/reference_genome.fna \
+    -R ../reference_genome/reference_genome.fna \
     -V SRR064545.vcf.gz \
     -V SRR064546.vcf.gz \
     -V SRR064547.vcf.gz \
@@ -29,13 +29,13 @@ gatk CombineGVCFs \
 
 echo -e "\nGenotyping..."
 gatk GenotypeGVCFs \
-    -R /home/abrzoza/Pulpit/reference_genome/reference_genome.fna \
+    -R ../reference_genome/reference_genome.fna \
     -V merged.vcf.gz \
     -O merged_genotyped.vcf.gz
 
 echo -e "\nSelecting SNPs..."
 gatk SelectVariants \
-    -R /home/abrzoza/Pulpit/reference_genome/reference_genome.fna \
+    -R ../reference_genome/reference_genome.fna \
     -V merged_genotyped.vcf.gz \
     --select-type-to-include SNP \
     -O SNP.vcf.gz
