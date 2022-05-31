2cca80182166de4cfa48d523a794998acf7c479c
Anita Brzoza <anita_brzoza@wp.pl>
2022-05-31 22:13:10 +0200

    Add annotation_and_comparative_analysis.R

---
+++ NGS_data_analysis/src/annotation_and_comparative_analysis.R
@@ -0,0 +1,41 @@
+
+#===============================================================================
+#
+# This script contains commands to annotate SNP and comparative analysis 
+# of samples.
+# 
+#===============================================================================
+
+## Libraries
+library(VariantAnnotation)
+library(GenomicFeatures)
+library(ggplot2)
+
+## annotating SNP ==============================================================
+## Geting data
+vcf <- readVcf("D:/github/university_projects/NGS_data_analysis/data/NC_001144.5.recode.vcf")
+annotation_info <- makeTxDbFromGFF("C:/Users/pourp/OneDrive/Pulpit/annotation.gff")
+
+# Annotating
+annotated_variants <- locateVariants(vcf, annotation_info, AllVariants())
+
+## Comparative analysis of samples =============================================
+# What genotypes are present
+genotypes <- geno(vcf)$GT
+
+# Finding in which regions and genes the gene variants occur
+unique_location_of_variants <- unique(annotated_variants@elementMetadata$LOCATION)
+
+unique(annotated_variants@elementMetadata$GENEID)
+
+variants_location <- data.frame(
+  Chromosome=annotated_variants@seqnames@values,
+  Position=annotated_variants@ranges@start,
+  Genome_region=annotated_variants@elementMetadata$LOCATION,
+  Gene_ID=annotated_variants@elementMetadata$GENEID
+)
+
+
+
+
+
