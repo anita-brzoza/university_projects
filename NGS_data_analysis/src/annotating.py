073ba615ce1fcf41a14861a1328f6d8cfa5441f0
Anita Brzoza <anita_brzoza@wp.pl>
2022-05-31 08:22:08 +0200

    deleting files annotating.py and filtering_SNP.sh

--- NGS_data_analysis/src/annotating.py
+++
@@ -1,8 +0,0 @@
-#================================================================================
-#
-# This script contains command to annotate VCF file using GFF file.
-#
-#================================================================================
-
-from bioinfokit.analys import marker
-marker.vcf_anot(file='./SNP.vcf', gff_file='./annotation.gff')
